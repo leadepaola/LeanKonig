0ca98a024a3646462dca823ad6089213c07f5dae
Leandro <leandrogdepaola@gmail.com>
2025-01-15 07:01:50 -0300

    Update style.php

--- proyects/PR/lib/base/css/style.php
+++ proyects/PR/lib/base/css/style.php
@@ -48,7 +48,7 @@
 	.pr_content_general_img_fondo{
 
 		width: 100%;
-		height: 100vh;
+		height: 120vh;
 
 		object-fit: cover;
 		filter: blur(1px)!important;
@@ -87,8 +87,8 @@
 
 	.pr_content_secundario_img_fondo{
 		width: 100%;
-		height: 100vh;
-		
+		height: 120vh;
+
 		object-fit: cover;
 		border-radius: 20px 20px 0px 0px;
 	}
